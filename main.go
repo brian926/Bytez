04d616ded748c51a7778909b054c09be65ee5191
Brian Antunes <brian.antunes@yahoo.com>
2022-12-04 14:35:57 -0500

    Added URL generator

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"github.com/brian926/UrlShorterGo/handler"
+	"github.com/brian926/UrlShorterGo/store"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,9 +12,19 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hey GO URL Shortener",
+			"message": "Hey Weclome to the URL Shortener API",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
