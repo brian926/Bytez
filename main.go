70e5ff431e389263e5d9f44b67f9b7d901a3854f
Brian Antunes <brian.antunes@yahoo.com>
2022-12-06 00:01:26 -0500

    Added ui for shortener

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/brian926/UrlShorterGo/handler"
 	"github.com/brian926/UrlShorterGo/store"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{http.MethodGet, http.MethodPatch, http.MethodPost, http.MethodHead, http.MethodDelete, http.MethodOptions},
+		AllowHeaders:     []string{"Content-Type", "X-XSRF-TOKEN", "Accept", "Origin", "X-Requested-With", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	r.GET("/", func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
 		c.JSON(200, gin.H{
 			"message": "Hey Weclome to the URL Shortener API",
 		})
