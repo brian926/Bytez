ab19542980c5678917b80f747d57273713d5f739
Brian Antunes <brian.antunes@yahoo.com>
2022-12-17 08:10:21 -0500

    Added to frontend

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Bytez
+
 A URL Shortener to deliever fast short URLs!
 
 ## About
+
 This was created to create a full stack site that delievers short URLs fast!
 Using Go and Redis, we can write a fast backend and store short URLs in Redis cache. Then we can store users and URLs within Postgres for long term shortage.
 
-
 ## Technologies Used
-Made with **Go**, **React**, **Redis**, **Postgres**
 
-## TODO:
-- Finish frontend
+Made with **Go**, **React**, **Redis**, **Vite**, **Postgres**, & **Docker**
+
+## TODO
+
+- Remove Users from app
 - Save URLs to postgres
-- Clean Redis cache after time duration
+- Check if key in Redis cache, else pull from database
+- Add local storage
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,139 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-// import Register from './pages/Register'
-// import Login from './pages/Login'
-import Session from './pages/Session'
+import React, { useState, useEffect } from "react"
+import { Endpoints } from "./api"
+import Errors from "./components/Errors"
 import './App.css'
 
 function App() {
+  const [item, setItems] = useState([])
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('items'))
+    if (items) {
+      console.log(item)
+      setItems(items)
+    }
+  }, [])
+
+  // Fetch welcome message, connecting to backend
+  const [isFetching, setIsFetching] = useState(false)
+  const [errors, setErrors] = useState([])
+
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const getData = async () => {
+        try {
+          setIsFetching(true)
+          const response = await fetch(
+          `http://localhost:9808`
+          );
+          if (!response.ok) {
+          throw new Error(
+              `This is an HTTP error: The status is ${response.status}`
+          );
+          }
+          let actualData = await response.json();
+          setData(actualData);
+          setError(null);
+        } catch(err) {
+            setError(err.message);
+            setData(null);
+        } finally {
+            setIsFetching(false)
+            setLoading(false);
+        }  
+        }
+        getData()
+    }, [])
+
+  // Handle URL creations
+  const fakeUserId = "e0dba740-fc4b-4977-872c-d360239e6b1a"
+  
+  const [post, setPost] = useState("") 
+  const [responseToPost, setState] = useState(null) 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(Endpoints.createUrl, {
+      method: 'POST',
+      body: JSON.stringify({"long_url":post, "user_id": fakeUserId})
+
+      })
+      if (!response.ok) {
+        throw new Error(
+          `This is an HTTP error: The status is ${response.status}`
+        );
+      }
+        let actualData = await response.json();
+
+        setState(actualData.short_url)
+        localStorage.setItem('items', JSON.stringify(actualData.short_url))
+    } catch(err) {
+        setErrors([e.toString()])
+        console.log(err)
+    }
+  } 
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Session />} />
-        <Route path="/session" element={<Session />} />
-      </Routes>
-    </Router>
+    <div className="wrapper">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/bytez-blue.png" className="bytez" alt="bytez" />
+          </a>
+        </div>
+        <div className="card">
+          <p><b>{data && (data.message)}</b></p>
+          {loading && <div>A moment please...</div>}
+          {error && (<div>{`There is a problem fetching the post data - ${error}`}</div>)}
+        </div>
+        <p>Enter a URL link down below and generate a shorter cleaner URL for easy sharing and posting!</p>
+        <p>*Links generated will only be avaialble for 6 hours*</p>
+      <div>
+        {isFetching ? (
+          <div>fetching details...</div>
+        ) : (
+          <div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                  <input 
+                    type="text"
+                    value={post}
+                    name="long_url"
+                    onChange={ e => setPost(e.target.value)}
+                  />
+                  <button type="submit">Generate</button>
+                </form>
+                {responseToPost && (<div> <a className="link" target="_blank" href={responseToPost}>{responseToPost}</a></div>)}
+              </div>
+          </div>
+        )}
+
+        <Errors errors={errors} />
+
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src="/react.svg" className="logo" alt="React logo" />
+          </a>
+          <a href="https://go.dev" target="_blank">
+            <img src="/go.svg" className="logo" alt="Go logo" />
+          </a>
+          <a href="https://redis.io" target="_blank">
+            <img src="/redis.svg" className="logo" alt="Redis logo" />
+          </a>
+          <a href="https://www.postgresql.org" target="_blank">
+            <img src="/postgres.svg" className="logo" alt="Postgres logo" />
+          </a>
+        </div>
+      </div>
+        <p className="read-the-docs">
+          Click on the Vite, React, Go, Redis, and Postgres logos to learn more
+        </p>
+    </div>
   )
 }
 
