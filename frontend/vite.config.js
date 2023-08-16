b7d44d8ef7e0be7083756f82c06a53b9a3df6a59
Brian Antunes <brian.antunes@yahoo.com>
2023-08-15 20:38:28 -0400

    changed host in vite and added env examples

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 5173,
   },
   plugins: [react()]
